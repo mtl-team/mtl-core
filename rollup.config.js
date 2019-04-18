62caf1c8cfa457e25f7c38495a9d8379db09ec84
guoyff <guoyff@yonyou.com>
2019-04-18 18:13:25 +0800

    uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
-export default [
-  {
+const env = process.env.NODE_ENV
+
+const config = {
     input: './src/mtl.js',
     output: {
       file: 'dist/mtl-core.js',
@@ -28,4 +30,18 @@ export default [
       commonjs()
     ]
   }
-];
+
+if (env === 'prod') {
+  config.plugins.push(
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
